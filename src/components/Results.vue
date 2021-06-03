3672980ba074479ea6315be53af5b51851fd18ca
Adarsh <aaa.narayanan@gmail.com>
2021-06-03 22:35:25 +0530

    Added main files

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="showresult">
+    <p>Reaction time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Flash fingers ⚡";
+    } else if (this.score < 400) {
+      this.rank = "Good Reflex ";
+    } else {
+      this.rank = "Slow snail";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf84;
+  font-weight: bold;
+}
+</style>
